a0dc9d4f1028325e594b4864a9761b613869e613
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-07-01 00:50:34 +0300

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Алексей Гуляев on 01.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
